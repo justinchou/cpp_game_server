8bdee1c27a4b206b080fd9dec0f84b845fff2c2f
justinchou <jackychou315@gmail.com>
2013-12-07 07:21:03 +0800

    new file:   template/COPYING 	new file:   template/FAQ 	new file:   template/INSTALL 	new file:   template/LICENSE 	new file:   template/Makefile 	new file:   template/NOTICE 	new file:   template/README.md 	new file:   template/docs/index.html 	new file:   template/makeTemplate.sh 	new file:   template/tests/main.cpp 	new file:   template/tests/tests.cpp

---
+++ template/tests/tests.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
+
